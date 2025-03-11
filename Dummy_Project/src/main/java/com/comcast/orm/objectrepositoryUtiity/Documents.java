aa2725fe8fc5b0fc7795237320bc71ba87d8e824
Admin <Admin@DESKTOP-0J1MG91>
2025-03-11 15:29:34 -0700

    both ele 1 and ele2

--- Dummy_Project/src/main/java/com/comcast/orm/objectrepositoryUtiity/Documents.java
+++ Dummy_Project/src/main/java/com/comcast/orm/objectrepositoryUtiity/Documents.java
@@ -8,13 +8,13 @@ public class Documents {
 	@FindBy(xpath="//img[@alt='Create Document...']")
 	private WebElement DocPlus;
 	
-<<<<<<< HEAD
+
 	@FindBy(xpath="seardchBtn")
 	private WebElement ele3;
-=======
+
 	@FindBy(xpath="search")
 	private WebElement ele2;
->>>>>>> branch 'master' of https://github.com/TejashwiniU-U/ComcastFrameworkRepo.git
+//github.com/TejashwiniU-U/ComcastFrameworkRepo.git
 	
 	
 }
